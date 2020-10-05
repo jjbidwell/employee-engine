4c057b9134f9aa2e09b63577d853d85ff6efce2a
Joshua Bidwell <jjbidwell262@gmail.com>
2020-10-05 15:45:42 -0700

    made employee only take name, id, email parameters and edited tests to reflect that.

--- test/employee.test.js
+++ test/employee.test.js
@@ -4,13 +4,10 @@ describe('Employee', () => {
     describe('Initialization', () => {
 
         it("should create an object with a name and age if provided valid arguments", () => {
-            const employee = new Employee('Josh', "engineer", 1, "jjbidwell262@gmail.com", "jjbidwell");
-
+            const employee = new Employee('Josh', 1, "jjbidwell262@gmail.com");
             expect(employee.name).toEqual("Josh");
-            expect(employee.title).toEqual("engineer");
             expect(employee.id).toEqual(1);
             expect(employee.email).toEqual("jjbidwell262@gmail.com");
-            expect(employee.github).toEqual("jjbidwell");
         });
 
         it('Should throw an error if not provided a name', () => {
@@ -21,16 +18,16 @@ describe('Employee', () => {
             expect(cb).toThrow(err);
         })
 
-        it('should throw an error if not provided a job title', () => {
-            const cb = () => new Employee("Josh");
-            const err = new Error(
-                "Expected parameter 'title' to be a non-empty string"
-            );
-            expect(cb).toThrow(err);
-        });
+        // it('should throw an error if not provided a job title', () => {
+        //     const cb = () => new Employee("Josh");
+        //     const err = new Error(
+        //         "Expected parameter 'title' to be a non-empty string"
+        //     );
+        //     expect(cb).toThrow(err);
+        // });
 
         it('should throw an error if not provided an employee id', () => {
-            const cb = () =>  new Employee("Josh", "engineer");
+            const cb = () =>  new Employee("Josh");
             const err = new Error(
               "Expected parameter 'id' to be a non-negative number"
             );
@@ -39,19 +36,19 @@ describe('Employee', () => {
         });
 
         it('should throw an error if not provided an email', () => {
-            const cb = () => new Employee("Josh", "engineer", 1);
+            const cb = () => new Employee("Josh", 1);
             const err = new Error(
                 "Expected parameter 'email' to be a non-empty string"
             );
             expect(cb).toThrow(err);
         });
 
-        it('should throw an error if not provided a github profile', () => {
-            const cb = () => new Employee("Josh", "engineer", 1, "jjbidwell262@gmail.com");
-            const err = new Error(
-                "Expected parameter 'github' to be a non-empty string"
-            );
-            expect(cb).toThrow(err);
-        });
+        // it('should throw an error if not provided a github profile', () => {
+        //     const cb = () => new Employee("Josh", "engineer", 1, "jjbidwell262@gmail.com");
+        //     const err = new Error(
+        //         "Expected parameter 'github' to be a non-empty string"
+        //     );
+        //     expect(cb).toThrow(err);
+        // });
     });
 });
