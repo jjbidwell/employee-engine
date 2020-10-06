4ee4afac3aec95af5b2daeb36376b2b8256c40a4
Joshua Bidwell <jjbidwell262@gmail.com>
2020-10-05 17:04:46 -0700

    Added and tested intern constructor

---
+++ test/intern.test.js
@@ -0,0 +1,36 @@
+const Intern = require('../lib/intern');
+
+describe('Intern', () => {
+    describe('Initialization', () => {
+        it('should create an object with name, id, email, and school', () => {
+            const intern = new Intern("name", 1, "sample@gmail.com", "University of California, San Diego");
+            expect(intern.name).toEqual("name");
+            expect(intern.id).toEqual(1);
+            expect(intern.email).toEqual("sample@gmail.com");
+            expect(intern.school).toEqual("University of California, San Diego");
+        });
+
+        it('Should throw an error if not provided a school', () => {
+            const cb = () => new Intern('name', 1, 'sample@gmail.com');
+            const err = new Error(
+                "Expected parameter 'school' to be a non-empty string"
+            );
+            expect(cb).toThrow(err);
+        });
+    });
+
+
+    describe('getSchool', () => {
+        it('should return the inputted school', () => {
+            const josh = new Intern('Josh', 1, 'jjbidwell262@gmail.com', "University of California, San Diego");
+            expect(josh.getSchool()).toEqual("University of California, San Diego")
+        })
+    })
+
+    describe('getRole', () => {
+        it('should return "Intern"', () => {
+            const josh = new Intern('Josh', 1, 'jjbidwell262@gmail.com', "University of California, San Diego");
+            expect(josh.getRole()).toEqual("Intern");
+        })
+    });
+});
